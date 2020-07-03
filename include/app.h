d56cfea3b597dd2c55e29472def00c1555eb9d92
Pedro Borges <pedro.hov.borges@gmail.com>
2020-07-03 17:06:42 -0300

    Add interactive menu

--- include/app.h
+++ include/app.h
@@ -15,8 +15,11 @@ struct app {
     void add();
     void add(const std::string message);
     void list_messages();
+    void search();
     void search(const std::string search);
     void load_config();
 };
+
+void open_menu(app* app);
 
 #endif
--- src/app.cpp
+++ src/app.cpp
@@ -40,6 +40,8 @@ int app::run(int argc, char* argv[]) {
             message.append(argv[i]);
         }
         search(message);
+    } else if (action == "interactive") {
+        open_menu(this);
     } else {
         return show_usage(argv[0]);
     }
@@ -49,7 +51,7 @@ int app::run(int argc, char* argv[]) {
 
 void app::add() {
     std::string message;
-    std::cout << "Informe a mensagem:" << std::endl;
+    std::cout << "Message to add: " << std::ends;
     std::getline(std::cin, message);
 
     add(message);
@@ -60,19 +62,30 @@ void app::add(const std::string content) {
 
     diary.add_message(message);
     diary.dump_messages();
+
+    std::cout << "\n" << "Message added!!" << std::endl;
 }
 
 void app::list_messages() {
-    for (size_t i = 0; i < diary.messages_count; ++i) {
-        const Message& message = diary.messages[i];
+    std::cout << "# List of messages (" << this->diary.messages_count << ")\n" << std::endl;
+    for (size_t i = 0; i < this->diary.messages_count; ++i) {
+        const Message& message = this->diary.messages[i];
         std::cout << "-" << message.content << std::endl;
     }
+}
+
+void app::search() {
+    std::string search;
+    std::cout << "Search keyword: " << std::ends;
+    std::getline(std::cin, search);
+
+    this->search(search);
 }
 
 void app::search(const std::string search) {
     std::vector<Message*> messages = diary.search(search);
 
-    std::cout << messages.size() << " mensagens encontradas!" << std::endl;
+    std::cout << "\n#" << messages.size() << " messages found!" << "\n" << std::endl;
 
     if (messages.empty()) {
         return;
@@ -118,10 +131,47 @@ void app::load_config() {
         }
     }
 
+    this->diary.load_messages();
+
     file.close();
 }
 
 int app::show_usage(const std::string &prog_name) {
     std::cout << "Uso: " << prog_name << " add <mensagem>" << std::endl;
     return 1;
+}
+
+void open_menu(app* app) {
+    std::cout << "Diary 1.0" << "\n\n" << std::endl;
+    std::cout << "Select an action below: " << "\n\n" << std::endl;
+    std::cout << "1) List messages" << "\n" << std::endl;
+    std::cout << "2) Add a new message" << "\n" << std::endl;
+    std::cout << "3) Search all messages" << "\n\n" << std::endl;
+    std::cout << "0) End" << "\n\n" << std::endl;
+    std::cout << "Type here: " << std::ends;
+
+    std::string choice;
+    std::getline(std::cin, choice);
+
+    // skip 1 line to show response
+    std::cout << "\n" << std::endl;
+
+    if (!std::isdigit(choice[0])) {
+        std::cout << "Invalid option. Terminating..." << std::endl;
+        return;
+    }
+
+    int selected = std::stoi(choice);
+
+    // terminate
+    if (selected == 0) {
+        std::cout << "Terminating..." << std::endl;
+        return;
+    } else if (selected == 1) {
+        app->list_messages();
+    } else if (selected == 2) {
+        app->add();
+    } else if (selected == 3) {
+        app->search();
+    }
 }
